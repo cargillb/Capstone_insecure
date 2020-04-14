6cd4cc89320743da51a7b898ae4de85efd7089ba
cargillb <56117629+cargillb@users.noreply.github.com>
2020-04-14 15:32:32 -0400

    base.html (navbar) and home, tasks.html

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
-from flask import request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 webapp = Flask(__name__)
 
 @webapp.route('/')
-def home():
-    return render_template('home.html')
+@webapp.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        #Here we will want to check if there are matching username/password combos in the DB
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error = error)
+
+#def list_lists(user_id):        #home page displays a list of users lists
+#    db_connection = connect_to_database
+#    query = "SELECT 'name' FROM lists WHERE user_id = '{user_id}'"
+#    execute_query(db_connection, query).fetchall()
+#    context = {
+
+def login():
+    return render_template('login.html')
+
+#def home_page():
+#    context = {'list_lists(user_id)':)
