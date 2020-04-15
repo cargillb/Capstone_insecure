c9d57f5d95e700f763deb2cbe85b266340f62577
Tim Glew <38923544+Toranagason@users.noreply.github.com>
2020-04-15 16:27:32 -0400

    Read functionality implemented

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -17,19 +17,40 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-#def list_lists(user_id):        #home page displays a list of users lists
-#    db_connection = connect_to_database
-#    query = "SELECT 'name' FROM lists WHERE user_id = '{user_id}'"
-#    execute_query(db_connection, query).fetchall()
-#    context = {
 
-def login():
-    return render_template('login.html')
+@webapp.route('/home/<user_id>')
+def home(user_id):
+    """
+    Route for the home page of a user where all of their to-do lists will be listed
+    """
+    context = {}  # create context dictionary
+    db_connection = connect_to_database()  # connect to db
 
-#def home_page():
-#    context = {'list_lists(user_id)':)
+    query = "SELECT `username` FROM users WHERE `user_id` ='{}'".format(user_id)  # get username
+    rtn = execute_query(db_connection, query).fetchall()  # run query
+    context = {'user_name': rtn[0][0], 'user_id': user_id}
 
-#def view_list():
+    query = "SELECT * FROM `lists` WHERE `user_id` ='{}'".format(user_id)  # get list info for a user
+    rtn = execute_query(db_connection, query).fetchall()  # run query
+    context['rows'] = rtn  # rtn = list data
 
-#def logout():
+    return render_template('home.html', context=context)
 
+
+@webapp.route('/tasks/<list_id>')
+def tasks(list_id):
+    """
+    Route for the tasks page of a user's list where all of the tasks of a to do list are shown
+    """
+    context = {}  # create context dictionary
+    db_connection = connect_to_database()  # connect to db
+
+    query = "SELECT `name`, `description` FROM lists WHERE `list_id` ='{}'".format(list_id)  # get name/desc of list
+    rtn = execute_query(db_connection, query).fetchall()  # run query
+    context = {'list_name': rtn[0][0], 'list_desc': rtn[0][1]}
+
+    query = "SELECT * FROM `tasks` WHERE `list_id` ='{}'".format(list_id)  # get info of tasks on list
+    rtn = execute_query(db_connection, query).fetchall()  # run query
+    context['rows'] = rtn  # rtn = tasks data
+
+    return render_template('tasks.html', context=context)
