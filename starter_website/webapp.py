976185409b16bde5fbfa31309ba6d452a7633a65
Tim Glew <38923544+Toranagason@users.noreply.github.com>
2020-04-16 12:22:06 -0400

    CRUD for Lists (Home) implemented

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from db_connector.db_connector import connect_to_database, execute_query
+import sys  # to print to stderr
 #create the web application
 webapp = Flask(__name__)
 #webapp = Flask(__name__, static_url_path='/static')
@@ -54,3 +55,28 @@ def tasks(list_id):
     context['rows'] = rtn  # rtn = tasks data
 
     return render_template('tasks.html', context=context)
+
+
+@webapp.route('/add_list', methods=['POST'])
+def add_list():
+    """
+    Route to execute query to add lists to db
+    """
+    db_connection = connect_to_database()
+    inputs = request.form.to_dict(flat=True)  # get form inputs from request
+
+    query = "INSERT INTO `lists` (`user_id`, `name`, `description`) VALUES ({}, '{}', '{}');".format(inputs['user_id'], inputs['list_name'], inputs['list_desc'])
+    execute_query(db_connection, query).fetchall()  # execute query
+
+    return redirect("/home/" + inputs['user_id'])
+
+
+@webapp.route('/delete_list/<user_id>/<list_id>')
+def delete_list(user_id, list_id):
+    """
+    Route to delete a list
+    """
+    db_connection = connect_to_database()
+    query = "DELETE FROM `lists` WHERE `list_id` = '{}'".format(list_id)
+    execute_query(db_connection, query).fetchall()
+    return redirect('/home/' + user_id)
