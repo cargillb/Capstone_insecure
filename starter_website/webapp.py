b67bd631880c9917d4839c78c581be50d357c296
Tim Glew <38923544+Toranagason@users.noreply.github.com>
2020-04-16 15:49:04 -0400

    Fixed apostrophe bug

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -5,6 +5,8 @@ import sys  # to print to stderr
 webapp = Flask(__name__)
 #webapp = Flask(__name__, static_url_path='/static')
 
+
+#-------------------------------- Login Routes --------------------------------
 @webapp.route('/')
 @webapp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,6 +21,7 @@ def login():
     return render_template('login.html', error=error)
 
 
+#-------------------------------- Home (List) Routes --------------------------------
 @webapp.route('/home/<user_id>')
 def home(user_id):
     """
@@ -38,6 +41,32 @@ def home(user_id):
     return render_template('home.html', context=context)
 
 
+@webapp.route('/add_list', methods=['POST'])
+def add_list():
+    """
+    Route to execute query to add lists to db
+    """
+    db_connection = connect_to_database()
+    inputs = request.form.to_dict(flat=True)  # get form inputs from request
+
+    query = "INSERT INTO `lists` (`user_id`, `name`, `description`) VALUES ('{}', \"{}\", \"{}\")".format(inputs['user_id'], inputs['list_name'], inputs['list_desc'])
+    execute_query(db_connection, query).fetchall()  # execute query
+
+    return redirect("/home/" + inputs['user_id'])
+
+
+@webapp.route('/delete_list/<user_id>/<list_id>')
+def delete_list(user_id, list_id):
+    """
+    Route to delete a list
+    """
+    db_connection = connect_to_database()
+    query = "DELETE FROM `lists` WHERE `list_id` = '{}'".format(list_id)
+    execute_query(db_connection, query).fetchall()
+    return redirect('/home/' + user_id)
+
+
+#-------------------------------- Task Routes --------------------------------
 @webapp.route('/tasks/<list_id>')
 def tasks(list_id):
     """
@@ -57,31 +86,6 @@ def tasks(list_id):
     return render_template('tasks.html', context=context)
 
 
-@webapp.route('/add_list', methods=['POST'])
-def add_list():
-    """
-    Route to execute query to add lists to db
-    """
-    db_connection = connect_to_database()
-    inputs = request.form.to_dict(flat=True)  # get form inputs from request
-
-    query = "INSERT INTO `lists` (`user_id`, `name`, `description`) VALUES ({}, '{}', '{}');".format(inputs['user_id'], inputs['list_name'], inputs['list_desc'])
-    execute_query(db_connection, query).fetchall()  # execute query
-
-    return redirect("/home/" + inputs['user_id'])
-
-
-@webapp.route('/delete_list/<user_id>/<list_id>')
-def delete_list(user_id, list_id):
-    """
-    Route to delete a list
-    """
-    db_connection = connect_to_database()
-    query = "DELETE FROM `lists` WHERE `list_id` = '{}'".format(list_id)
-    execute_query(db_connection, query).fetchall()
-    return redirect('/home/' + user_id)
-
-
 @webapp.route('/add_task', methods=['POST'])
 def add_task():
     """
@@ -90,7 +94,7 @@ def add_task():
     db_connection = connect_to_database()
     inputs = request.form.to_dict(flat=True)  # get form inputs from request
 
-    query = "INSERT INTO `tasks` (`list_id`, `dataType_id`, `description`, `completed`) VALUES ('{}', '{}', '{}', '{}')".format(inputs['list_id'], inputs['task_type'], inputs['task_desc'], inputs['task_comp'])
+    query = "INSERT INTO `tasks` (`list_id`, `dataType_id`, `description`, `completed`) VALUES ('{}', '{}', \"{}\", '{}')".format(inputs['list_id'], inputs['task_type'], inputs['task_desc'], inputs['task_comp'])
     execute_query(db_connection, query).fetchall()  # execute query
 
     return redirect("/tasks/" + inputs['list_id'])
